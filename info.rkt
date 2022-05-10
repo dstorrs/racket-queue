98ebd99a4d3d849aef90c31ed9e874126aedde84
dstorrs <dstorrs.public@gmail.com>
2022-05-10 18:05:01 -0400

    info.rkt version bump

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "test-more"))
 (define scribblings '(("scribblings/queue.scrbl" ())))
 (define pkg-desc "A FIFO queue struct with amortized O(n) access")
-(define version "0.2")
+(define version "0.3")
 (define pkg-authors '("David K. Storrs"))
 (define license '(Apache-2.0 OR MIT))
